a3d624c291f87d6e000d4ab0c0952e6d448adad5
Tsebek <badmaev.t@gmail.com>
2024-02-20 20:58:48 -0800

    added question #7

---
+++ 7_Laptop vs. Mobile Viewership/7_solution.sql
@@ -0,0 +1,15 @@
+SELECT
+    SUM(
+      CASE
+        WHEN device_type = 'laptop'
+          THEN 1
+        ELSE 0
+      END) as laptop_views,
+    SUM(
+      CASE
+        WHEN device_type IN ('tablet', 'phone')
+          THEN 1
+        ELSE 0
+      END) as mobile_views
+FROM viewership
+;
